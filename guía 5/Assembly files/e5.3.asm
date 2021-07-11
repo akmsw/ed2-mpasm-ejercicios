b7fae916231d17915818edf4eb50018784ee3a91
Akamai <66426042+akmsw@users.noreply.github.com>
2021-07-11 19:51:24 -0300

    Update e5.3.asm

--- guía 5/Assembly files/e5.3.asm
+++ guía 5/Assembly files/e5.3.asm
@@ -1,12 +1,12 @@
-;   Ejercicio 5.3: (LA CONSIGNA SE CAMBIÓ PARA SIMPLICIDAD)
+;   Ejercicio 5.3: (LA CONSIGNA SE CAMBIÃ“ PARA SIMPLICIDAD)
 ;
-;   Escribir un programa que lea de dos botones conectados a RB1 y RB2 y actúe
+;   Escribir un programa que lea de dos botones conectados a RB1 y RB2 y actÃºe
 ;   sobre un LED conectado en RB3.
 ;
 ;   * Si se presiona RB1, se enciende el LED por 10 segundos y luego se apaga.
 ;   * Si se presiona RB2, se enciende el LED por 5 segundos y luego se apaga.
 ;
-;   Cualquier cambio en los botones mientras esté encendido el LED no deberá
+;   Cualquier cambio en los botones mientras estÃ© encendido el LED no deberÃ¡
 ;   modificar el estado del LED.
 	
 ;-------------------LIBRERIAS---------------------------------------------------
@@ -27,7 +27,7 @@
 	    V2	    EQU	    0x22    ; Variable para el loop medio.
 	    V3	    EQU	    0x23    ; Variable para el loop externo.
    
-;-------------------INICIALIZACIÓN----------------------------------------------
+;-------------------INICIALIZACIÃ“N----------------------------------------------
 	    
 	    ORG	    0x00
 	    
@@ -35,7 +35,7 @@
 	    
 	    ORG	    0x04
 	    
-	    GOTO    RUT_INT	    ; Cuando ocurre una interrupción, voy a la
+	    GOTO    RUT_INT	    ; Cuando ocurre una interrupciÃ³n, voy a la
 				    ; rutina de la misma.
 
 ;-------------------CONFIGURACION DE REGISTROS----------------------------------
@@ -53,7 +53,7 @@
 	    MOVWF   INTCON
 	    BANKSEL IOCB
 	    MOVLW   b'00000110'	    ; Habilito RB<2:1> como fuentes de
-	    MOVWF   IOCB	    ; interrupción
+	    MOVWF   IOCB	    ; interrupciÃ³n
 	    BANKSEL PORTB	    ; Vuelvo al banco de PORTB para comenzar.
 	    MOVLW   b'00000110'	    ; Limpio los registros a utilizar para
 	    MOVWF   PORTB	    ; evitar basura.
@@ -67,62 +67,62 @@
 ;-------------------INICIO DEL PROGRAMA-----------------------------------------
 	    
    START    GOTO    $		    ; El programa principal no hace nada.
-				    ; Espero a que ocurra una interrupción.
+				    ; Espero a que ocurra una interrupciÃ³n.
 				    
-;-------------------RUTINA DE INTERRUPCIÓN--------------------------------------
+;-------------------RUTINA DE INTERRUPCIÃ“N--------------------------------------
 	    
- RUT_INT    BTFSC   INTCON,RBIF	    ; Si la interrupción fue por PORTB, hago un
-	    CALL    BIT_TEST	    ; testeo para ver de qué bit vino la
+ RUT_INT    BTFSC   INTCON,RBIF	    ; Si la interrupciÃ³n fue por PORTB, hago un
+	    CALL    BIT_TEST	    ; testeo para ver de quÃ© bit vino la
 	    BCF	    INTCON,RBIF
-	    RETFIE		    ; interrupción.
+	    RETFIE		    ; interrupciÃ³n.
 	    
 BIT_TEST    MOVFW   PORTB	    ; Guardo el estado de PORTB en AUX para no
-	    MOVWF   AUX		    ; perder información.
+	    MOVWF   AUX		    ; perder informaciÃ³n.
 	    BTFSS   AUX,1	    ; Testeo RB1...
-	    CALL    LED_RB1	    ; Si está en 0 (se presionó), voy a LED_RB1.
+	    CALL    LED_RB1	    ; Si estÃ¡ en 0 (se presionÃ³), voy a LED_RB1.
 	    BTFSS   AUX,2	    ; Sino, testeo RB2...
-	    CALL    LED_RB2	    ; Si está en 0 (se presionó), voy a LED_RB2.
-	    RETURN		    ; Si la interrupción fue por otro bit de
+	    CALL    LED_RB2	    ; Si estÃ¡ en 0 (se presionÃ³), voy a LED_RB2.
+	    RETURN		    ; Si la interrupciÃ³n fue por otro bit de
 				    ; PORTB, no nos interesa, y vuelvo.
 				    
- LED_RB1    BSF	    PORTB,3	    ; Si la interrupción fue por RB1, enciendo
+ LED_RB1    BSF	    PORTB,3	    ; Si la interrupciÃ³n fue por RB1, enciendo
 	    CALL    TIMER_10S	    ; el LED 10 segundos, bajo la flag y vuelvo.
 	    BCF	    PORTB,3
 	    BCF	    INTCON,RBIF
 	    RETFIE
 	    
- LED_RB2    BSF	    PORTB,3	    ; Si la interrupción fue por RB1, enciendo
+ LED_RB2    BSF	    PORTB,3	    ; Si la interrupciÃ³n fue por RB1, enciendo
 	    CALL    TIMER_5S	    ; el LED 5 segundos, bajo la flag y vuelvo.
 	    BCF	    PORTB,3
 	    BCF	    INTCON,RBIF
 	    RETFIE
 	    
 TIMER_10S   MOVLW   .51		    ; Cargo V1, V2 y V3 con valores previamente
-	    MOVWF   V3		    ; calculados para que el ciclo dure 1[s].
+	    MOVWF   V3		    ; calculados para que el ciclo dure 10[s].
   LMED10    MOVLW   .255
 	    MOVWF   V2
   LINT10    MOVLW   .255
 	    MOVWF   V1
-	    DECFSZ  V1		    ; Decremento V1. Si aún no es cero...
+	    DECFSZ  V1		    ; Decremento V1. Si aÃºn no es cero...
 	    GOTO    $-1		    ; sigo decrementando V1.
 	    DECFSZ  V2		    ; Si V1 es cero, decremento V2.
-	    GOTO    LINT10	    ; Si V2 aún no es cero, recargo V1 y repito.
+	    GOTO    LINT10	    ; Si V2 aÃºn no es cero, recargo V1 y repito.
 	    DECFSZ  V3		    ; Si V2 es cero, decremento V3.
-	    GOTO    LMED10	    ; Si V3 aún no es cero, recargo V2 y repito.
+	    GOTO    LMED10	    ; Si V3 aÃºn no es cero, recargo V2 y repito.
 	    RETURN
 	    
 TIMER_5S    MOVLW   .26		    ; Cargo V1, V2 y V3 con valores previamente
-	    MOVWF   V3		    ; calculados para que el ciclo dure 1[s].
+	    MOVWF   V3		    ; calculados para que el ciclo dure 5[s].
    LMED5    MOVLW   .255
 	    MOVWF   V2
    LINT5    MOVLW   .255
 	    MOVWF   V1
-	    DECFSZ  V1		    ; Decremento V1. Si aún no es cero...
+	    DECFSZ  V1		    ; Decremento V1. Si aÃºn no es cero...
 	    GOTO    $-1		    ; sigo decrementando V1.
 	    DECFSZ  V2		    ; Si V1 es cero, decremento V2.
-	    GOTO    LINT5	    ; Si V2 aún no es cero, recargo V1 y repito.
+	    GOTO    LINT5	    ; Si V2 aÃºn no es cero, recargo V1 y repito.
 	    DECFSZ  V3		    ; Si V2 es cero, decremento V3.
-	    GOTO    LMED5	    ; Si V3 aún no es cero, recargo V2 y repito.
+	    GOTO    LMED5	    ; Si V3 aÃºn no es cero, recargo V2 y repito.
 	    RETURN
 	    
 	    END
