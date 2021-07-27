7c5718ae48ac3cf08498f707ab5e496e56fbdd94
Akamai <66426042+akmsw@users.noreply.github.com>
2021-07-27 11:58:43 -0300

    Update e4.6.asm

--- guía 4/Assembly files/e4.6.asm
+++ guía 4/Assembly files/e4.6.asm
@@ -1,7 +1,7 @@
 ;   Ejercicio 4.6:
 ;
 ;   Considerando el mismo hardware del ejercicio 4.4, escribir un programa que
-;   ilumine los LEDs conectados a PORTD según las siguientes especificaciones:
+;   ilumine los LEDs conectados a PORTD segÃºn las siguientes especificaciones:
 ;
 ;   * Inicialmente (al salir del reset), aparecen parpadeando los LEDs.
 ;   * Si se aprieta el pulsador conectado al pin RA4, se produce un
@@ -48,8 +48,8 @@
 	    BANKSEL ANSELH
 	    CLRF    ANSELH
 	    BANKSEL PORTA	    ; Vuelvo al banco de PORTA.
-	    CLRF    AUX		    ; Limpio el valor de toggleo de dirección y
-	    CLRF    PORTA	    ; también los registros a leer para evitar 
+	    CLRF    AUX		    ; Limpio el valor de toggleo de direcciÃ³n y
+	    CLRF    PORTA	    ; tambiÃ©n los registros a leer para evitar 
 	    CLRF    PORTB	    ; trabajar con basura.
 	    CLRF    PORTD
 	    CLRW
@@ -60,19 +60,19 @@
   TOGGLE    COMF    PORTD,1	; Complemento el valor de PORTD.
 	    CALL    TIMER_2S	; Espero dos segundos.
 	    BTFSS   PORTA,4	; Chequeo el estado de RA4.
-	    GOTO    PRESSEDA	; Si se apretó, hago el antirrebote.
+	    GOTO    PRESSEDA	; Si se apretÃ³, hago el antirrebote.
 	    GOTO    TOGGLE	; Y vuelvo.
 	    
 PRESSEDA    BTFSS   FLAG,0	; Chequeo si fue la primera vez en apretar RA4.
-	    CALL    FIRST	; Si sí, cargo PORTD por primera vez.
-	    BTFSS   PORTA,4	; Cuando el estado de RA4 cambie, continúo.
+	    CALL    FIRST	; Si sÃ­, cargo PORTD por primera vez.
+	    BTFSS   PORTA,4	; Cuando el estado de RA4 cambie, continÃºo.
 	    GOTO    $-1
 	    INCF    AUX		; Incremento la cantidad de veces que se
-	    BTFSC   AUX,1	; presionó RA4 y en base a eso toggleo la
-	    GOTO    LTOR	; dirección de desplazamiento de los LEDs.
+	    BTFSC   AUX,1	; presionÃ³ RA4 y en base a eso toggleo la
+	    GOTO    LTOR	; direcciÃ³n de desplazamiento de los LEDs.
 	    GOTO    RTOL
 	    
-PRESSEDB    BTFSS   PORTB,0	; Cuando el estado de RB0 cambie, continúo.
+PRESSEDB    BTFSS   PORTB,0	; Cuando el estado de RB0 cambie, continÃºo.
 	    GOTO    $-1
 	    CLRF    AUX		; Limpio todos los registros utilizados y
 	    CLRF    FLAG	; vuelvo al estado inicial de toggleo.
@@ -81,23 +81,23 @@ PRESSEDB    BTFSS   PORTB,0	; Cuando el estado de RB0 cambie, continúo.
 	    
     RTOL    CALL    TIMER_2S	; Espero dos segundos.
     	    BTFSS   PORTA,4	; Chequeo el estado de RA4.
-	    GOTO    PRESSEDA	; Si se apretó, hago el antirrebote.
+	    GOTO    PRESSEDA	; Si se apretÃ³, hago el antirrebote.
 	    BTFSS   PORTB,0	; Chequeo el estado de RB0.
-	    GOTO    PRESSEDB	; Si se apretó, hago el antirrebote.
+	    GOTO    PRESSEDB	; Si se apretÃ³, hago el antirrebote.
 	    RLF	    PORTD	; Roto PORTD hacia la izquierda.
-	    BTFSC   STATUS,C	; Si el carry está en '1' (di toda la vuelta),
-	    CALL    LOAD	; recargo PORTD y continúo.
+	    BTFSC   STATUS,C	; Si el carry estÃ¡ en '1' (di toda la vuelta),
+	    CALL    LOAD	; recargo PORTD y continÃºo.
 	    GOTO    RTOL
 	    
     LTOR    CLRF    AUX		; Reseteo AUX.
 	    CALL    TIMER_2S	; Espero dos segundos.
 	    BTFSS   PORTA,4	; Chequeo el estado de RA4.
-	    GOTO    PRESSEDA	; Si se apretó, hago el antirrebote.
+	    GOTO    PRESSEDA	; Si se apretÃ³, hago el antirrebote.
 	    BTFSS   PORTB,0	; Chequeo el estado de RB0.
-	    GOTO    PRESSEDB	; Si se apretó, hago el antirrebote.
+	    GOTO    PRESSEDB	; Si se apretÃ³, hago el antirrebote.
 	    RRF	    PORTD	; Roto PORTD hacia la derecha.
-	    BTFSC   STATUS,C	; Si el carry está en '1' (di toda la vuelta),
-	    CALL    LOAD2	; recargo PORTD y continúo.
+	    BTFSC   STATUS,C	; Si el carry estÃ¡ en '1' (di toda la vuelta),
+	    CALL    LOAD2	; recargo PORTD y continÃºo.
 	    GOTO    LTOR
 	    
 TIMER_2S    NOP			; --SUBRUTINA DE TIEMPO DE DOS SEGUNDOS--
@@ -107,12 +107,12 @@ TIMER_2S    NOP			; --SUBRUTINA DE TIEMPO DE DOS SEGUNDOS--
 	    MOVWF   V2
    LINT2    MOVLW   .255
 	    MOVWF   V1
-	    DECFSZ  V1		; Decremento V1. Si aún no es cero...
+	    DECFSZ  V1		; Decremento V1. Si aÃºn no es cero...
 	    GOTO    $-1		; sigo decrementando V1.
 	    DECFSZ  V2		; Si V1 es cero, decremento V2.
-	    GOTO    LINT2	; Si V2 aún no es cero, recargo V1 y repito.
+	    GOTO    LINT2	; Si V2 aÃºn no es cero, recargo V1 y repito.
 	    DECFSZ  V3		; Si V2 es cero, decremento V3.
-	    GOTO    LEXT2	; Si V3 aún no es cero, recargo V2 y repito.
+	    GOTO    LEXT2	; Si V3 aÃºn no es cero, recargo V2 y repito.
 	    RETURN		; Si V3 es cero, salgo de la subrutina.
 	    
    FIRST    INCF    FLAG	; Si es la primera vez pulsando RA4, se setea
@@ -130,11 +130,11 @@ TIMER_2S    NOP			; --SUBRUTINA DE TIEMPO DE DOS SEGUNDOS--
 	    RETURN
 	    
 ;-------------------COMENTARIOS-------------------------------------------------
-;
-;	Este ejercicio, al igual que el 4.5, está hecho de manera muy tosca
-;	por el hecho de que en la unidad 4 no se abarcan las interrupciones.
-;	Para poder apreciar correctamente el comportamiento del PIC en la
-;	simulación en Proteus, hay que hacer lo mismo que en el ejercicio
-;	4.5. Ver la sección COMENTARIOS del ejercicio 4.5 para entender mejor.
+
+;   Este ejercicio, al igual que el 4.5, estÃ¡ hecho de manera muy tosca por el
+;   hecho de que en la unidad 4 no se abarcan las interrupciones. Para poder
+;   apreciar correctamente el comportamiento del PIC en la simulaciÃ³n en
+;   Proteus, hay que hacer lo mismo que en el ejercicio 4.5. Ver la secciÃ³n
+;   COMENTARIOS del ejercicio 4.5 para entender mejor.
 	    
 	    END
