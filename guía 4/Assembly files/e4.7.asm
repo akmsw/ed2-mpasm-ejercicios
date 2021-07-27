5cce03164a4aec62a702ec3ceabdd931ddab9cba
Akamai <66426042+akmsw@users.noreply.github.com>
2021-07-27 11:59:39 -0300

    Update e4.7.asm

--- guía 4/Assembly files/e4.7.asm
+++ guía 4/Assembly files/e4.7.asm
@@ -1,10 +1,10 @@
 ;   Ejercicio 4.7:
 ;
 ;   Realizar un programa en lenguaje Assembly que cuente de 0 a 9
-;   indefinidamente. Cada n˙mero permanecer· encendido un segundo (retardo por
-;   software). El conteo iniciar· en 0 al apretarse un pulsador en RB0 y se
-;   detendr· al volver a pulsarlo en el valor que estÈ la cuenta. La salida
-;   ser· por PORTD. El oscilador es de 4[MHz].
+;   indefinidamente. Cada n√∫mero permanecer√° encendido un segundo (retardo por
+;   software). El conteo iniciar√° en 0 al apretarse un pulsador en RB0 y se
+;   detendr√° al volver a pulsarlo en el valor que est√© la cuenta. La salida
+;   ser√° por PORTD. El oscilador es de 4[MHz].
 
 ;-------------------LIBRERIAS---------------------------------------------------
 
@@ -21,7 +21,7 @@
 	    
 	    ORG	    0x00
 	    
-	    D7S_0   EQU	    0x20    ; Registros que almacenar·n los valores
+	    D7S_0   EQU	    0x20    ; Registros que almacenar√°n los valores
 	    D7S_1   EQU	    0x21    ; binarios para encender los LEDs de un
 	    D7S_2   EQU	    0x22    ; display de 7 segmentos contando desde
 	    D7S_3   EQU	    0x23    ; 0 hasta 9.
@@ -71,7 +71,7 @@
 
 ;-------------------INICIO DEL PROGRAMA-----------------------------------------
 	    
-    POLL    BTFSS   PORTB,0	    ; Chequeo el estado de RB0. Si est·
+    POLL    BTFSS   PORTB,0	    ; Chequeo el estado de RB0. Si est√°
 	    GOTO    PRESSED_B0	    ; presionado, voy al antirrebote.
 	    GOTO    POLL
 	    
@@ -79,31 +79,31 @@ PRESSED_B0  BTFSS   PORTB,0
 	    GOTO    $-1
 	    GOTO    COUNT
 	    
-   COUNT    GOTO    SET_FSR	    ; Cargo FSR con la primera posiciÛn de
+   COUNT    GOTO    SET_FSR	    ; Cargo FSR con la primera posici√≥n de
     LOOP    MOVFW   INDF	    ; memoria con los valores para mostrar en
 	    MOVWF   PORTD	    ; PORTD. Voy mostrando por PORTD esperando
 	    CALL    TIMER_1S	    ; un segundo.
-	    MOVFW   FSR		    ; Si lleguÈ a la posiciÛn 0x29, reseteo el
+	    MOVFW   FSR		    ; Si llegu√© a la posici√≥n 0x29, reseteo el
 	    SUBLW   0x29	    ; contador.
 	    BTFSC   STATUS,Z
 	    GOTO    SET_FSR
 	    INCF    FSR
-	    BTFSS   PORTB,0	    ; Chequeo el estado de RB0. Si est·
+	    BTFSS   PORTB,0	    ; Chequeo el estado de RB0. Si est√°
 	    GOTO    PRESSED_B02	    ; presionado, hago un nuevo polling sin
 	    GOTO    LOOP	    ; resetear la cuenta.
 	    
 PRESSED_B02 BTFSC   PORTB,0	    ; Cuando RB0 cambia de estado, voy a un
-	    GOTO    POLL2	    ; polling de pausa que luego me llevar· a
+	    GOTO    POLL2	    ; polling de pausa que luego me llevar√° a
 	    GOTO    PRESSED_B02	    ; LOOP en vez de a COUNT, para no resetar
 				    ; el valor del registro FSR.
 	    
-   POLL2    BTFSS   PORTB,0	    ; Chequeo el estado de RB0. Si est·
+   POLL2    BTFSS   PORTB,0	    ; Chequeo el estado de RB0. Si est√°
 	    GOTO    PRESSED_B03	    ; presionado, voy al nuevo antirrebote.
 	    GOTO    POLL2
 	    
 PRESSED_B03 BTFSC   PORTB,0	    ; En este tercer antirrebote voy a LOOP y no
 	    GOTO    LOOP	    ; a COUNT para que el contador siga desde
-	    GOTO    PRESSED_B03	    ; donde quedÛ el registro FSR.
+	    GOTO    PRESSED_B03	    ; donde qued√≥ el registro FSR.
 	    
 TIMER_1S    MOVLW   .6		    ; Cargo V1, V2 y V3 con valores previamente
 	    MOVWF   V3		    ; calculados para que el ciclo dure 1[s].
@@ -111,12 +111,12 @@ TIMER_1S    MOVLW   .6		    ; Cargo V1, V2 y V3 con valores previamente
 	    MOVWF   V2
     LINT    MOVLW   .255
 	    MOVWF   V1
-	    DECFSZ  V1		    ; Decremento V1. Si a˙n no es cero...
+	    DECFSZ  V1		    ; Decremento V1. Si a√∫n no es cero...
 	    GOTO    $-1		    ; sigo decrementando V1.
 	    DECFSZ  V2		    ; Si V1 es cero, decremento V2.
-	    GOTO    LINT	    ; Si V2 a˙n no es cero, recargo V1 y repito.
+	    GOTO    LINT	    ; Si V2 a√∫n no es cero, recargo V1 y repito.
 	    DECFSZ  V3		    ; Si V2 es cero, decremento V3.
-	    GOTO    LMED	    ; Si V3 a˙n no es cero, recargo V2 y repito.
+	    GOTO    LMED	    ; Si V3 a√∫n no es cero, recargo V2 y repito.
 	    RETURN		    ; Si V3 es cero, vuelvo.
 	    
  SET_FSR    MOVLW   0x20
@@ -125,11 +125,11 @@ TIMER_1S    MOVLW   .6		    ; Cargo V1, V2 y V3 con valores previamente
 	    
 ;-------------------COMENTARIOS-------------------------------------------------
 ;
-;	Una vez m·s, este ejercicio tiene complicaciones por no trabajar con
-;	interrupciones. Si bien la pulsaciÛn que comienza el conteo funciona
-;	correctamente, la pulsaciÛn que lo detiene tiene la misma particularidad
-;	que los ejercicios 4.6 y 4.5: hay que mantener presionado el botÛn hasta
-;	que el PIC registre el cambio de estado por polling y luego se lo puede
-;	soltar para continuar con la ejecuciÛn del programa.
+;   Una vez m√°s, este ejercicio tiene complicaciones por no trabajar con
+;   interrupciones. Si bien la pulsaci√≥n que comienza el conteo funciona
+;   correctamente, la pulsaci√≥n que lo detiene tiene la misma particularidad
+;   que los ejercicios 4.6 y 4.5: hay que mantener presionado el bot√≥n hasta que
+;   el PIC registre el cambio de estado por polling y luego se lo puede soltar
+;   para continuar con la ejecuci√≥n del programa.
 	    
 	    END
