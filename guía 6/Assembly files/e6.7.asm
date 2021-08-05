89e10ae216250166a3840256f88a0d6371815f38
Akamai <66426042+akmsw@users.noreply.github.com>
2021-08-05 01:51:46 -0300

    Update e6.7.asm

--- guía 6/Assembly files/e6.7.asm
+++ guía 6/Assembly files/e6.7.asm
@@ -1,11 +1,11 @@
-;   Ejercicio 6.7: (LA CONSIGNA SE CAMBIÓ PARA COMPLETITUD)
+;   Ejercicio 6.7: (LA CONSIGNA SE CAMBIÃ“ PARA COMPLETITUD)
 ;
 ;   Conectar a un PIC un teclado matricial 4x4 y un arreglo de 6 displays de 7
-;   segmentos de ánodo común. Considere que los segmentos se activan por 0.
-;   Almacenar en un buffer circular de 6 posiciones las últimas 6 teclas que se
+;   segmentos de Ã¡nodo comÃºn. Considere que los segmentos se activan por 0.
+;   Almacenar en un buffer circular de 6 posiciones las Ãºltimas 6 teclas que se
 ;   hayan presionado y mostrarlo por los displays (un display por registro). Al
-;   comenzar, como los registros estarán en '0', los correspondientes displays
-;   estarán apagados hasta que el registro se sobrescriba. A continuación se
+;   comenzar, como los registros estarÃ¡n en '0', los correspondientes displays
+;   estarÃ¡n apagados hasta que el registro se sobrescriba. A continuaciÃ³n se
 ;   muestra el reemplazo de valores en el teclado KEYPAD-SMALLCALC ofrecido por
 ;   Proteus:
 ;
@@ -37,9 +37,9 @@
 	  MAX_ROW   EQU	    0x25    ; columnas del teclado.
 	     D_ON   EQU	    0x26    ; Display a encender.
 	    D_QUA   EQU	    0x27    ; Cantidad total de displays.
-	 BUFF_INI   EQU	    0x28    ; Dirección inicial del buffer circular.
-	 BUFF_LIM   EQU	    0x29    ; Dirección final del buffer circular + 1.
-	   BUFF_W   EQU	    0x2A    ; Dirección del buffer a escribir.
+	 BUFF_INI   EQU	    0x28    ; DirecciÃ³n inicial del buffer circular.
+	 BUFF_LIM   EQU	    0x29    ; DirecciÃ³n final del buffer circular + 1.
+	   BUFF_W   EQU	    0x2A    ; DirecciÃ³n del buffer a escribir.
 	       V1   EQU	    0x2B    ; Variables para el retardo por software.
 	       V2   EQU	    0x2C
 	      AUX   EQU	    0x2D    ; Variable auxiliar para escritura.
@@ -62,13 +62,13 @@
 	    CLRF    D_ON	    ; Vamos a trabajar con 6 displays y vamos a
 	    MOVLW   0x06	    ; comenzar por el display 0.
 	    MOVWF   D_QUA
-	    MOVLW   0x37	    ; El buffer tendrá 6 registros.
+	    MOVLW   0x36	    ; El buffer tendrÃ¡ 6 registros.
 	    MOVWF   BUFF_LIM
 	    CLRF    COLUMN	    ; Limpio registros a usar para evitar
 	    CLRF    ROW		    ; basura de ejecuciones anteriores.
 	    CLRF    COL_HAB
 	    CLRF    COL_HAB_AUX
-	    MOVLW   0x30	    ; El buffer circular comenzará en el
+	    MOVLW   0x30	    ; El buffer circular comenzarÃ¡ en el
 	    MOVWF   BUFF_INI	    ; registro 0x30.
 	    MOVWF   BUFF_W
 	    BANKSEL TRISB	    ; Seteo RB<0:3> como outputs digitales, y
@@ -93,7 +93,7 @@
 	    MOVLW   0xF0
 	    MOVWF   IOCB
 	    BANKSEL PORTC	    ; Vuelvo al banco de PORTC para comenzar.
-	    CALL    CLEAR_BUFF	    ; Limpio el buffer para que comience vacío.
+	    CALL    CLEAR_BUFF	    ; Limpio el buffer para que comience vacÃ­o.
 	    CLRF    PORTB
 	    CLRF    PORTC
 	    CLRF    PORTD
@@ -101,8 +101,8 @@
 	    GOTO    INIT
 	    
 CLEAR_BUFF  MOVFW   BUFF_INI	    ; Seteo a 0xFF todos los bytes del buffer
-	    MOVWF   FSR		    ; por estar trabajando con displays de ánodo
-CONTINUE    CLRF    INDF	    ; común.
+	    MOVWF   FSR		    ; por estar trabajando con displays de Ã¡nodo
+CONTINUE    CLRF    INDF	    ; comÃºn.
 	    COMF    INDF
 	    INCF    FSR,F
 	    MOVFW   FSR
@@ -141,7 +141,7 @@ CONTINUE    CLRF    INDF	    ; común.
  
 ;-------------------TABLAS------------------------------------------------------
     
-D_SELECT    ADDWF   PCL,F	    ; Tabla para elegir qué display encender.
+D_SELECT    ADDWF   PCL,F	    ; Tabla para elegir quÃ© display encender.
 	    RETLW   B'00000001'
 	    RETLW   B'00000010'
 	    RETLW   B'00000100'
@@ -173,9 +173,9 @@ ROWS_TEST   ADDWF   PCL,F
 	    RETLW   B'11111011'
 	    RETLW   B'11110111'
 	    
-;-------------------RUTINA DE INTERRUPCIÓN--------------------------------------
+;-------------------RUTINA DE INTERRUPCIÃ“N--------------------------------------
 	    
-  RUT_IN    BTFSS   INTCON,RBIF	    ; Sólo atiendo interrupciones por RB<4:7>.
+  RUT_IN    BTFSS   INTCON,RBIF	    ; SÃ³lo atiendo interrupciones por RB<4:7>.
 	    RETFIE
 	    MOVFW   PORTB	    ; Decodifico lo pulsado como en el ejercicio
 	    ANDLW   0xF0	    ; 6.3a.
@@ -215,44 +215,40 @@ SHOW_RES    BCF	    STATUS,C
 	    CALL    SAVE_
 	    GOTO    FINISH
 	    
-   SAVE_    MOVWF   AUX			; Guardo momentáneamente en AUX lo que
-	    MOVFW   BUFF_W		; se decodificó para guardarlo donde
+   SAVE_    MOVWF   AUX			; Guardo momentÃ¡neamente en AUX lo que
+	    MOVFW   BUFF_W		; se decodificÃ³ para guardarlo donde
 	    MOVWF   FSR			; corresponde.
 	    MOVFW   AUX
-	    MOVWF   INDF		; Lo que se pulsó se guarda en el buffer
+	    MOVWF   INDF		; Lo que se pulsÃ³ se guarda en el buffer
 	    INCF    BUFF_W,F		; y se incrementa el registro a escribir
-	    MOVFW   BUFF_W		; chequeando no sobrepasar el límite.
+	    MOVFW   BUFF_W		; chequeando no sobrepasar el lÃ­mite.
 	    SUBWF   BUFF_LIM,W
 	    BTFSC   STATUS,Z
 	    CALL    RESET_BUFF
 	    RETURN
 	    
 RESET_BUFF  MOVFW   BUFF_INI		; Resteo el buffer. La siguiente
-	    MOVWF   BUFF_W		; posición a escribir será aquella en la
+	    MOVWF   BUFF_W		; posiciÃ³n a escribir serÃ¡ aquella en la
 	    RETURN			; que comienza el buffer.
 
   FINISH    CLRF    PORTB		; Limpio la flag y reseteo los valores
-	    MOVFW   PORTB		; de fila y columna para la próxima
-    	    BCF	    INTCON,RBIF		; decodificación (ver COMENTARIOS).
+	    MOVFW   PORTB		; de fila y columna para la prÃ³xima
+    	    BCF	    INTCON,RBIF		; decodificaciÃ³n (ver COMENTARIOS).
 	    CLRF    ROW
 	    CLRF    COLUMN
 	    RETFIE
 	    
 ;-------------------COMENTARIOS-------------------------------------------------
 
-;   De acuerdo con la documentación y con la prueba y error, para poder limpiar
+;   De acuerdo con la documentaciÃ³n y con la prueba y error, para poder limpiar
 ;   correctamente la flag RBIF se debe primero limpiar PORTB, luego hacer una
 ;   lectura/escritura sobre el mismo puerto y luego limpiar RBIF. Caso contrario
-;   la flag no se limpiará y eso causará errores en la implementación del
-;   código.
-;   Como comentario, hay dos bugs que no he podido identificar en el código, y
-;   que sólo se pueden apreciar en las simulaciones en Proteus:
-;
-;   1)	Cuando el programa arranca, la primer pulsación en el teclado no se
-;	toma, recién a la segunda pulsada se comienza a escribir en el buffer.
-;   2)	Cuando se llega a la última posición del buffer (todos los displays han
-;	sido escritos), ocurre nuevamente el bug #1: la primera pulsada no se
-;	registra, pero la siguiente sí.
+;   la flag no se limpiarÃ¡ y eso causarÃ¡ errores en la implementaciÃ³n del
+;   cÃ³digo.
+;   Como comentario, hay un bug que no he podido identificar en el cÃ³digo, y
+;   que sÃ³lo se puede apreciar en las simulaciones en Proteus: cuando el
+;   programa arranca, la primer pulsaciÃ³n en el teclado no se toma, reciÃ©n a la
+;   segunda pulsada se comienza a escribir en el buffer.
 ;
 ;   Fuera de esto, el programa funciona correctamente.
  
